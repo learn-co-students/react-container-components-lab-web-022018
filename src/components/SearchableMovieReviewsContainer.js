d9366f433239fe576f548cbfa58f12011aca7106
alec-petros <a.petros@verizon.net>
2018-04-25 16:06:39 -0400

    Done.

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,33 @@
 // Code SearchableMovieReviewsContainer Here
+import React from 'react';
+import MovieReviews from './MovieReviews'
+
+class SearchableMovieReviewsContainer extends React.Component {
+
+  state = {
+    searchVal: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({searchVal: e.target.value})
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault()
+    this.props.searchSubmit(this.state.searchVal)
+  }
+
+  render() {
+    return(
+      <div class="search-movie-container">
+        <form onSubmit={this.handleSearch}>
+          <input onChange={this.handleChange} value={this.state.searchVal} type="text" />
+          <input type="submit" />
+        </form>
+        {this.props.reviews.map((review, index) => {return (<MovieReviews key={index} data={review}/>)})}
+      </div>
+    )
+  }
+}
+
+export default SearchableMovieReviewsContainer

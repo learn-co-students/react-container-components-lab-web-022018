97baef7f98d1135aa9fd3f21465eac2036ea8cc9
github username <github email address>
2018-04-25 15:41:54 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -7,7 +7,7 @@ const MovieReviews = (props)=>{
   };
 
   console.log(props.reviews)
-  const reviewList = props.reviews.map((review, index)=>{return <p key={index}>{review.byline +" | " + review.display_title +" | " + review.mpaa_rating}</p>})
+  const reviewList = props.reviews.map((review, index)=>{return <p className="review" key={index}>{review.byline +" | " + review.display_title +" | " + review.mpaa_rating}</p>})
 
   console.log(reviewList)
     return(

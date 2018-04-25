145bcd3600cb4c9cd7b15662d220d2183a30c940
github username <github email address>
2018-04-25 09:10:55 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
+import MovieReviews from './MovieReviews'
 import 'isomorphic-fetch';
+require('es6-promise').polyfill();
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+
+const NYT_API_KEY = '4e5a46d2c2974c6c9e2185b06e132c54';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component{
+
+  constructor(){
+    super()
+
+    this.state = {
+      reviews: []
+    }
+  }
+
+
+  componentDidMount(){
+    fetch(URL).then(res=>res.json()).then(json=>{
+      this.setState({
+        reviews: json.results
+      })
+    })
+  }
+
+
+  render(){
+    return (
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+}
+
+export default LatestMovieReviewsContainer
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,66 @@
-// Code MovieReviews Here
+import React from 'react'
+//
+// class MovieReviews extends React.Component{
+//
+//   render(){
+//     return (
+//       <div className="review-list">
+//         {this.props.reviews.map(review=>
+//           (
+//             <div>
+//             <h5>This movie review:</h5>
+//             <h1>{review.display_title}</h1>
+//             <h3>{review.mpaa_rating}</h3>
+//             <h3>{review.critics_pick}</h3>
+//             <h3>{review.byline}</h3>
+//             <h3>{review.headline}</h3>
+//             <h3>{review.summary_short}</h3>
+//             </div>
+//           )
+//         )}
+//       </div>
+//     )
+//   }
+// }
+
+const MovieReviews = ({reviews}) => {
+  return <div className="review-list">
+  {reviews.map(review=>
+    (
+      <div>
+      <h5>This movie review:</h5>
+      <h1>{review.display_title}</h1>
+      <h3>{review.mpaa_rating}</h3>
+      <h3>{review.critics_pick}</h3>
+      <h3>{review.byline}</h3>
+      <h3>{review.headline}</h3>
+      <h3>{review.summary_short}</h3>
+      </div>
+    )
+  )}
+  </div>
+}
+//
+// const reviewRender = ({reviews}) =>{
+//   return (
+//     <div>
+//       <h5>This movie review:</h5>
+//       <h1>{reviews.display_title}</h1>
+//       <h3>{reviews.mpaa_rating}</h3>
+//       <h3>{reviews.critics_pick}</h3>
+//       <h3>{reviews.byline}</h3>
+//       <h3>{reviews.headline}</h3>
+//       <h3>{reviews.summary_short}</h3>
+//     </div>
+//   )
+// }
+//
+//
+// const MovieReviews = ({reviews}) =>  <div className="review-list"> {reviews.map(reviewRender)}</div>
+
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews

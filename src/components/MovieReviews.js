0af5e4a00abb7bdad2f3e666e4ef366f5af6aafb
sinclairstudy <sinclairstudy@gmail.com>
2018-04-26 01:20:29 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Review = (props) => (
   <div className="review">
-    <a href={props.link.url}>{props.display_title}</a>
+    <a href={props.link.url} target="_blank">{props.display_title}</a>
   </div>
 )
 
